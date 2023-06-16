2d115791c6e0f8dea8dc10846056e357c1899912
alpinn <alfinasyah36@gmail.com>
2023-06-16 19:32:32 +0700

    add dropdown component, make a payment method page, anda add headlessUI

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,21 @@
 import Navbar from '@/components/layouts/Navbar';
 import Hero from '@/components/Hero';
 import FavoriteDestination from '@/components/FavoriteDestination';
+import NavbarPayment from '@/components/payments/NavbarPayment';
+import PaymentMethod from '@/components/payments/PaymentMethod';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Hero />
       <FavoriteDestination />
-      {/* <SearchModal/>
+      <SearchModal/>
       <DateModal/>
-      <PriceModal/> */}
+      <PriceModal/>  */}
+      <NavbarPayment/>
+      <PaymentMethod/>
+      
     </>   
   )
 }
---
+++ components/Drowdown.jsx
@@ -0,0 +1,113 @@
+'use client'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import Image from "next/image";
+import PaymentOpt from "@/assets/images/paymentopt.png"
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(' ')
+// }
+
+const Dropdown = () => {
+  return (
+    <>
+        <Menu as="div" className="relative block text-left pb-10">
+            <div>
+                <Menu.Button className="inline-flex w-[30.3rem] h-[50px] gap-x-1.5 rounded-md bg-neutral-4 px-3 py-3.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300">
+                Gopay
+                <ChevronDownIcon className="ml-auto h-5 w-5 text-gray-400" aria-hidden="true" />
+                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                    <input type="text" />
+                </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+        <Menu as="div" className="relative block text-left pb-10">
+            <div>
+                <Menu.Button className="inline-flex w-[30.3rem] h-[50px] gap-x-1.5 rounded-md bg-neutral-4 px-3 py-3.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300">
+                Virtual Account
+                <ChevronDownIcon className="ml-auto h-5 w-5 text-gray-400" aria-hidden="true" />
+                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                    <input type="text" />
+                </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+        <Menu as="div" className="relative block text-left pb-10">
+            <div>
+                <Menu.Button className="inline-flex w-[30.3rem] h-[50px] gap-x-1.5 rounded-md bg-neutral-4 px-3 py-3.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300">
+                Credit Card
+                <ChevronDownIcon className="ml-auto h-5 w-5 text-gray-400" aria-hidden="true" />
+                </Menu.Button>
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute left-0 z-10 mt-2 w-[30.3rem] origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Image
+                        src={PaymentOpt}
+                        alt = "Payment Option"
+                        className="mx-auto py-4 mb-2"
+                    />
+                    <div className="px-20 ">
+                        <div className="px-2 pb-7">
+                            <label htmlFor="">Card number</label>
+                            <input type="text" className="block border-b-2 w-[18.5rem] focus:border-0"/>
+                        </div>
+                        <div className="px-2 pb-7">
+                            <label htmlFor="">Card holder name</label>
+                            <input type="text" className="block border-b-2 w-[18.5rem] focus:border-0"/>
+                        </div>
+                        <div className="px-2 pb-7 mb-3 flex flex-wrap gap-[2.05rem]">
+                            <div>
+                                <label htmlFor="">CVV</label>
+                                <input type="text" className="block border-b-2 w-[8.25rem] focus:border-0"/>
+                            </div>
+                            <div>
+                                <label htmlFor="">Expiry date</label>
+                                <input type="text" className="block border-b-2 w-[8.25rem] focus:border-0"/>
+                            </div>
+                        </div>
+                    </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
+    </>
+  )
+}
+
+export default Dropdown;
