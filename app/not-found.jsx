4d21b65f8c9ec52ffdc02da0781871eb4b67ba01
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2023-07-06 11:46:16 +0700

    feat: add not found ui

---
+++ app/not-found.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import flightTicketsNotFound from "@/assets/images/flight/tickets-not-found.svg";
+import ButtonLink from "@/components/atoms/ButtonLink";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-6 min-h-screen w-full">
+      <Image src={flightTicketsNotFound} alt="Flight tickets not found" />
+      <ButtonLink 
+        href="/" 
+        size="md" 
+        variant="primary" 
+        className="px-4 rounded-xl"
+      >
+        Back to Home
+      </ButtonLink>
+    </div>
+  )
+}
+
+export default NotFound
